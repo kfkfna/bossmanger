666d404f9edede2430fcb2236fda272c4854e05a
projectmanger <2415796068@qq.com>
2019-11-21 19:24:25 +0800

    文件说明

--- sdai.js
+++ sdai.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+require('./model/connect');
+// require('./model/user');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// 模板引擎相关配置
+app.engine('art', require('express-art-template'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'art');
+
+// 静态资源访问
+app.use(express.static(path.join(__dirname, 'public')));
+
+const home = require('./route/home');
+const admin = require('./route/admin');
+
+// 前台相关的路由
+app.use('/admin', admin);
+// 后台相关的路由
+app.use('/home', home);
+
+app.listen(3000);
